f373d668cafe161a880165e618229cd552d2528f
dimonser147 <dimonser147@gmail.com>
2017-03-27 15:34:31 +0300

    Improve texts on a home page

--- src/GeoWebApp/Utilities/Rules/WwwRule.cs
+++ src/GeoWebApp/Utilities/Rules/WwwRule.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Rewrite;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace GeoWebApp.Utilities.Rules
 {
